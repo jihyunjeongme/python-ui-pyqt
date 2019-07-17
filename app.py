cbeb9fb586c673a30fc80e5e114ffe401f51a728
jihyunjeongme <jihyunjeong.me@gmail.com>
2019-07-17 19:15:40 +0900

    UI initial version

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
-import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit)
+# Reference
+# https://wikidocs.net/book/2165
+# https://wikidocs.net/35492
 
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+import time
+import datetime
+import random
+from selenium import webdriver
 
 class MyApp(QWidget):
-
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.btn1 = QPushButton("&Like", self)
 
-    def initUI(self):
+        self.lbl1 = QLabel("ID", self)
+        self.lbl1.move(40, 99)
 
-        grid = QGridLayout()
-        self.setLayout(grid)
+        self.lbl2 = QLabel("PW", self)
+        self.lbl2.move(40, 129)
 
-        grid.addWidget(QLabel('Title:'), 0, 0)
-        grid.addWidget(QLabel('Author:'), 1, 0)
-        grid.addWidget(QLabel('Review:'), 2, 0)
+        self.qle1 = QLineEdit(self)
+        self.qle1.move(60, 100)
 
-        grid.addWidget(QLineEdit(), 0, 1)
-        grid.addWidget(QLineEdit(), 1, 1)
-        grid.addWidget(QTextEdit(), 2, 1)
+        self.qle2 = QLineEdit(self)
+        self.qle2.setEchoMode(QLineEdit.Password)
 
-        self.setWindowTitle('QGridLayout')
-        self.setGeometry(300, 300, 300, 200)
+        self.qle2.move(60, 130)
+
+        self.setWindowTitle("Instagram Auto Program")
+        # x, y, width, height
+        self.setGeometry(300, 300, 300, 300)
         self.show()
 
+        # 버튼에 기능을 할당하는 코드
+        self.btn1.clicked.connect(self.printTextEdit)
+        # self.initUI()
 
-if __name__ == '__main__':
+    def printTextEdit(self):
+        print(self.qle1.text())
+        print(self.qle2.text())
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = MyApp()
     sys.exit(app.exec_())
